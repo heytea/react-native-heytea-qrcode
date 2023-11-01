874ecbc3f01ffbb817ca742aac5507c09c1f7997
ChrisZhou <37390688+chris6605@users.noreply.github.com>
2023-11-01 13:43:47 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { NativeModules,NativeEventEmitter,Platform } from "react-native";
 const { HeyTeaQRCode } = NativeModules;
 const QRCodeManagerEmitter = new NativeEventEmitter(HeyTeaQRCode);
 
-export const scanQRCode = () => {
+export const scanQRCode = (lang) => {
     const {scanQRCode} = HeyTeaQRCode
     if (Platform.OS === 'android') {
-      return scanQRCode()
+      return scanQRCode(lang)
     }else {
-        scanQRCode()
+        scanQRCode(lang)
         return new Promise((resolve) => {
             QRCodeManagerEmitter.addListener(
                 'ScanQRCodeInfoNotification',
